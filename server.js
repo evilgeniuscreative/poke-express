245df8a4cf9519be121683e28ca7bb78ef4c78df
Jurgen Stevens <jurgen.stevens@yahoo.com>
2021-06-08 17:37:37 -0500

    Update server.js
    Erased code and left pseudocode so there are no merge conflicts.

--- server.js
+++ server.js
@@ -1,9 +1,4 @@
 // set up the requirements ie express
-const express = require('express')
-const app = express()
-const PORT = 8000
+
 
 // set up the listener
-app.listen(PORT, () => {
-    console.log('Connected to the Poke Express on ')
-})
