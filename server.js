a2ba33fec0de35fc0bc9cec43a5d4e538c196d7f
iansexton <mydesignguy@gmail.com>
2023-11-01 22:31:29 -0400

    Commit 3 - created pokemon controller and refactored server

--- server.js
+++ server.js
@@ -1,11 +1,44 @@
-// set up the requirements ie express
 const express = require('express');
 const app = express();
 
-// set up the port
-const PORT = process.env.PORT || 3000;
 
-// set up the listener
-app.listen(PORT, () => {
-  console.log(`App listening on PORT ${PORT}`);
-});
+
+
+
+//--------------  Import CSS &/or JSON ----------------//
+app.use(express.static('public'));
+
+console.log('dirname', __dirname);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+
+
+//--------------  Server  ----------------//
+
+const startServer = async () => {
+  // connect to DB
+  await mongoose.connect(DATABASE_URL);
+
+  mongoose.connection.on('connected', () => {
+    console.log('Connected to ' + DATABASE_URL);
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+  app.on('connected', () => {
+    console.log('MongoDB connected on:', DATABASE_URL);
+  });
+};
+
+
+//--------------  Routes Middleware  ----------------//
+
+const pokemonRouter = require('./routers/pokemon.js');
+
+app.use('/p', pokemonRouter);
+
+startServer();
