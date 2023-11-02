0cabdc64e8168d247d185fbe21f03dea95a89201
iansexton <mydesignguy@gmail.com>
2023-11-01 22:15:00 -0400

    Commit 1 - my server is up and running

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 // set up the requirements ie express
+const express = require('express');
+const app = express();
 
+// set up the port
+const PORT = process.env.PORT || 3000;
 
 // set up the listener
+app.listen(PORT, () => {
+  console.log(`App listening on PORT ${PORT}`);
+});
